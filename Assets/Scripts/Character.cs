bd00d3ada22a89a4e084c39cc36add1f2d8ae70d
Freddie Jones <freddie.jones2011@hotmail.co.uk>
2021-05-14 21:25:03 +0100

    Implemented basic character script

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,6 +8,33 @@ public class Character : MonoBehaviour
 
   private void Update()
   {
-    
+    if (isDodging) { return; }
+
+    if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      Jump();
+    }
+    else if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+      Slide();
+    }
+  }
+
+  private void Jump()
+  {
+    isDodging = true;
+
+    Debug.Log("Jumping");
+
+    isDodging = false;
+  }
+
+  private void Slide()
+  {
+    isDodging = true;
+
+    Debug.Log("Sliding");
+
+    isDodging = false;
   }
 }
